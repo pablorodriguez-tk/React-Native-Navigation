fc4f9e54005ba93c0c2a6cb76fa190793c63e4fb
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-06 22:10:12 -0300

    Initial setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App Screen</Text>
+    </View>
+  );
+};
+
+export default App;
