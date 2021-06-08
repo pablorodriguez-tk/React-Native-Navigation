aab36d4878a01b9abbd8458289efd698ed346931
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-08 16:54:11 -0300

    Add context

--- App.tsx
+++ App.tsx
@@ -4,16 +4,23 @@ import {NavigationContainer} from '@react-navigation/native';
 // import {StackNavigator} from './src/navigator/StackNavigator';
 // import {DrawerNavigator} from './src/navigator/DrawerNavigator';
 import {DrawerNavigator2} from './src/navigator/DrawerNavigator2';
+import {AuthProvider} from './src/context/AuthContext';
 // import {TabsNavigator} from './src/navigator/TabsNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <StackNavigator /> */}
-      <DrawerNavigator2 />
-      {/* <TabsNavigator /> */}
+      <AppState>
+        {/* <StackNavigator /> */}
+        <DrawerNavigator2 />
+        {/* <TabsNavigator /> */}
+      </AppState>
     </NavigationContainer>
   );
+};
+
+const AppState = ({children}: {children: JSX.Element}) => {
+  return <AuthProvider>{children}</AuthProvider>;
 };
 
 export default App;
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {AuthContext} from '../context/AuthContext';
 import {styles} from '../theme/appTheme';
 
 export const SettingsScreen = () => {
   const insets = useSafeAreaInsets();
+  const {authState} = useContext(AuthContext);
 
   return (
     <View style={{...styles.globalMargin, marginTop: insets.top + 20}}>
       <Text style={styles.title}>SettingsScreen</Text>
+      <Text>{JSON.stringify(authState, null, 4)}</Text>
     </View>
   );
 };
