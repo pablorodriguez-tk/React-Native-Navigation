4ffdc1b162f2206adfcba56d350acdb38ec36e3d
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-08 20:51:29 -0300

    Changing Favorite icon authState

--- src/components/TouchableIcon.tsx
+++ src/components/TouchableIcon.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {AuthContext} from '../context/AuthContext';
 import {colors} from '../theme/appTheme';
 
 interface Props {
@@ -8,8 +9,9 @@ interface Props {
 }
 
 export const TouchableIcon = ({iconName}: Props) => {
+  const {changeFavoriteIcon} = useContext(AuthContext);
   return (
-    <TouchableOpacity onPress={() => console.log(iconName)}>
+    <TouchableOpacity onPress={() => changeFavoriteIcon(iconName)}>
       <Icon name={iconName} size={80} color={colors.primary} />
     </TouchableOpacity>
   );
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,8 +1,9 @@
 import React, {useContext} from 'react';
 import {Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Icon} from 'react-native-vector-icons/Ionicons';
 import {AuthContext} from '../context/AuthContext';
-import {styles} from '../theme/appTheme';
+import {colors, styles} from '../theme/appTheme';
 
 export const SettingsScreen = () => {
   const insets = useSafeAreaInsets();
@@ -12,6 +13,9 @@ export const SettingsScreen = () => {
     <View style={{...styles.globalMargin, marginTop: insets.top + 20}}>
       <Text style={styles.title}>SettingsScreen</Text>
       <Text>{JSON.stringify(authState, null, 4)}</Text>
+      {authState.favoriteIcon && (
+        <Icon name={authState.favoriteIcon} size={150} color={colors.primary} />
+      )}
     </View>
   );
 };
